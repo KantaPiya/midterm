e69bda079c3b2d5974a3008b59ff74ed6280e611
KantaPiya <monaekanta9@gmail.com>
2024-12-23 14:54:43 +0700

    Add files via upload

---
+++ midterm37-509.py
@@ -0,0 +1,12 @@
+ความกว้าง=float(input())
+ความยาว=float(input())
+ความลึก=float(input())
+ปริมาตร=ความกว้าง*ความยาว*ความลึก
+ต้องใช้เวลา=(ปริมาตร/0.25)
+print(ต้องใช้เวลา)
+
+
+midterm = float(input(""))
+final = float(input(""))
+print("Total:",midterm + final )
+print("Average:",total / 2 )
